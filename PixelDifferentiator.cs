7ebb56b9141e3da6157fe377083a7d0d35c76864
Adam Langevin <langer1911@gmail.com>
2017-10-30 13:26:07 -0400

    differentiator first pass, might need more changes and to be moved'

--- PixelDifferentiator.cs
+++ PixelDifferentiator.cs
@@ -14,6 +14,10 @@ namespace CannyDetection
       b = src;
     }
 
+    /*Convolutional matrix operation, using the Sobel operators. This
+    referantially calculates the gradient of the Bitmap supplied to the calss.
+    the magic happens here.
+    */
     public static Bitmap differentiate()
     {
       double xr=0;
@@ -30,9 +34,14 @@ namespace CannyDetection
       int coff=0;
       int boff=0;
 
+      double[,] xKern = Sobelx();
+      double[,] yKern = Sobel;y();
+
       bData = b.LockBits(new Rectangle(0,0,b.Width,b.Height),
                         ImageLockMode.ReadOnly,
                         PixelFormat.Format32bppArgb);
+
+      byte[] result = new byte[bData.Stride * bData.Height];
 
       for(int offY=foff; offY < b.Height-foff; offY++)
       {
@@ -46,14 +55,40 @@ namespace CannyDetection
             {
               for(int j=-foff; j<=foff; j++)
               {
+                coff = boff + offX*4 + offY*bData.Stride;
 
+                xb += (double)(b[coff]) * xKern[i+foff, j+foff];
+                xg += (double)(b[coff+1]) * xKern[i+foff, j+foff];
+                xr += (double)(b[coff+2]) * xKern[i+foff, j+foff];
+                yb += (double)(b[coff]) * yKern[i+foff, j+foff];
+                yg += (double)(b[coff+1]) * yKern[i+foff, j+foff];
+                yr += (double)(b[cofff+2]) * yKern[i+foff, j+foff];
               }
             }
 
+            cb = Math.Sqrt((xb*xb) + (yb*yb));
+            cg = Math.Sqrt((xg*xg) + (yg*yg));
+            cr = Math.Sqrt((xr*xr) + (yr*yr));
+
+            if(cb>255)  cb=255;
+            elseif(cb<0)cb=0;
+            if(cg>255)  cg=255;
+            elseif(cg<0)cg=0;
+            if(cr>255)  cr=255;
+            elseif(cr<0)cr=0;
+
+            result[boff] = (byte)(cb);
+            result[boff +1] = (byte)(cg);
+            result[boff +2] = (byte)(cr);
+            result[boff +3] = 255;
           }
       }
+
+      Marshal.Copy(result, 0, bData.Scan0, result.Lenght);
+      return b.UnlockBits(bData);
     }
 
+    //Sobel matrix operator in the x plane
     private static double[,] Sobelx()
     {
       return d = new double[,] = {
@@ -62,7 +97,9 @@ namespace CannyDetection
                                   {-1,0,1}
                                   };
     }
-    private static double[,] Sobley()
+
+    //Sobel matrix operator in the y plane
+    private static double[,] Sobely()
     {
       return d = new double[,] = {
                                 {1,2,1},
