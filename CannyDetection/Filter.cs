e86a67f8e59a1ee8f92c5aeecfe212d16ae22a2d
Adam Langevin <langer1911@gmail.com>
2017-11-22 17:06:03 -0500

    almost finished Max suppresion, need to remove single points

--- CannyDetection/Filter.cs
+++ CannyDetection/Filter.cs
@@ -58,10 +58,10 @@ namespace CannyDetection
             bSrc = (Bitmap)b.Clone();
             BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
                                                         ImageLockMode.ReadWrite,
-                                                        PixelFormat.Format24bppRgb);
+                                                        PixelFormat.Format32bppArgb);
             BitmapData bmSrc = bSrc.LockBits(new Rectangle(0, 0, bSrc.Width, bSrc.Height),
                                                         ImageLockMode.ReadWrite,
-                                                        PixelFormat.Format24bppRgb);
+                                                        PixelFormat.Format32bppArgb);
             int stride = bmData.Stride;
             int stride2 = stride * 2;
 
@@ -82,53 +82,68 @@ namespace CannyDetection
                 {
                     for (int x = 0; x < nWidth; ++x)
                     {
+                        nPixel = ((((pSrc[3] * m.TopLeft) +
+                                       (pSrc[7] * m.TopMid) +
+                                       (pSrc[11] * m.TopRight) +
+                                       (pSrc[3 + stride] * m.MidLeft) +
+                                       (pSrc[7 + stride] * m.Pixel) +
+                                       (pSrc[11 + stride] * m.MidRight) +
+                                       (pSrc[3 + stride2] * m.BottomLeft) +
+                                       (pSrc[7 + stride2] * m.BottomMid) +
+                                       (pSrc[11 + stride2] * m.BottomRight))
+                                        / m.Factor) + m.Offset);
+
+                        if (nPixel < 0) nPixel = 0;
+                        if (nPixel > 255) nPixel = 255;
+                        p[7 + stride] = (byte)nPixel;
+
                         nPixel = ((((pSrc[2] * m.TopLeft) +
-                                    (pSrc[5] * m.TopMid) +
-                                    (pSrc[8] * m.TopRight) +
+                                    (pSrc[6] * m.TopMid) +
+                                    (pSrc[10] * m.TopRight) +
                                     (pSrc[2 + stride] * m.MidLeft) +
-                                    (pSrc[5 + stride] * m.Pixel) +
-                                    (pSrc[8 + stride] * m.MidRight) +
+                                    (pSrc[6 + stride] * m.Pixel) +
+                                    (pSrc[10 + stride] * m.MidRight) +
                                     (pSrc[2 + stride2] * m.BottomLeft) +
+                                    (pSrc[6 + stride2] * m.BottomMid) +
+                                    (pSrc[10 + stride2] * m.BottomRight))
+                                    / m.Factor) + m.Offset);
+
+                        if (nPixel < 0) nPixel = 0;
+                        if (nPixel > 255) nPixel = 255;
+                        p[6 + stride] = (byte)nPixel;
+
+                        nPixel = ((((pSrc[1] * m.TopLeft) +
+                                    (pSrc[5] * m.TopMid) +
+                                    (pSrc[9] * m.TopRight) +
+                                    (pSrc[1 + stride] * m.MidLeft) +
+                                    (pSrc[5 + stride] * m.Pixel) +
+                                    (pSrc[9 + stride] * m.MidRight) +
+                                    (pSrc[1 + stride2] * m.BottomLeft) +
                                     (pSrc[5 + stride2] * m.BottomMid) +
-                                    (pSrc[8 + stride2] * m.BottomRight))
+                                    (pSrc[9 + stride2] * m.BottomRight))
                                     / m.Factor) + m.Offset);
 
                         if (nPixel < 0) nPixel = 0;
                         if (nPixel > 255) nPixel = 255;
                         p[5 + stride] = (byte)nPixel;
 
-                        nPixel = ((((pSrc[1] * m.TopLeft) +
-                                    (pSrc[4] * m.TopMid) +
-                                    (pSrc[7] * m.TopRight) +
-                                    (pSrc[1 + stride] * m.MidLeft) +
-                                    (pSrc[4 + stride] * m.Pixel) +
-                                    (pSrc[7 + stride] * m.MidRight) +
-                                    (pSrc[1 + stride2] * m.BottomLeft) +
-                                    (pSrc[4 + stride2] * m.BottomMid) +
-                                    (pSrc[7 + stride2] * m.BottomRight))
-                                    / m.Factor) + m.Offset);
-
-                        if (nPixel < 0) nPixel = 0;
-                        if (nPixel > 255) nPixel = 255;
-                        p[4 + stride] = (byte)nPixel;
-
                         nPixel = ((((pSrc[0] * m.TopLeft) +
-                                       (pSrc[3] * m.TopMid) +
-                                       (pSrc[6] * m.TopRight) +
+                                       (pSrc[4] * m.TopMid) +
+                                       (pSrc[8] * m.TopRight) +
                                        (pSrc[0 + stride] * m.MidLeft) +
-                                       (pSrc[3 + stride] * m.Pixel) +
-                                       (pSrc[6 + stride] * m.MidRight) +
+                                       (pSrc[4 + stride] * m.Pixel) +
+                                       (pSrc[8 + stride] * m.MidRight) +
                                        (pSrc[0 + stride2] * m.BottomLeft) +
-                                       (pSrc[3 + stride2] * m.BottomMid) +
-                                       (pSrc[6 + stride2] * m.BottomRight))
+                                       (pSrc[4 + stride2] * m.BottomMid) +
+                                       (pSrc[8 + stride2] * m.BottomRight))
                                         / m.Factor) + m.Offset);
 
                         if (nPixel < 0) nPixel = 0;
                         if (nPixel > 255) nPixel = 255;
-                        p[3 + stride] = (byte)nPixel;
-
-                        p += 3;
-                        pSrc += 3;
+                        p[4 + stride] = (byte)nPixel;
+                       
+                        p += 4;
+                        pSrc += 4;
                     }
 
                     p += nOffset;
--- CannyDetection/MaximumSuppression.cs
+++ CannyDetection/MaximumSuppression.cs
@@ -11,6 +11,9 @@ namespace CannyDetection
 {
     class MaximumSuppression
     {
+        public static int[,] EdgeMap;
+        public static int[,] Visited;
+        public static int[,] Edges;
 
         public static Bitmap Suppression(Bitmap b)
         {
@@ -49,6 +52,9 @@ namespace CannyDetection
 
             divY = Filter.Conv(divY, m);
 
+            int[,] aDivX = convertToArray(divX);
+            int[,] aDivY = convertToArray(divY);
+
             BitmapData divXData = divX.LockBits(new Rectangle(0, 0, b.Width, b.Height),
                         ImageLockMode.ReadOnly,
                         PixelFormat.Format32bppArgb);
@@ -64,6 +70,8 @@ namespace CannyDetection
 
             float[,] Grade = new float[b.Width, b.Height];
             float[,] NMax = new float[b.Width, b.Height];
+            float[,] postHyst = new float[b.Width, b.Height];
+            Edges = new int[b.Width, b.Height];
 
             int strideX = divXData.Stride;
             int strideY = divYData.Stride;
@@ -102,47 +110,20 @@ namespace CannyDetection
                 float c1 = 0;     //center pixel to be differentiated against
                 float c2 = 0;     //test pixel one
                 float c3 = 0;     //test pixel two
-                int pixleSize = 4;  //standard pixel size, for calculations
 
                 for (int y=lim; y<= b.Height-1; ++y)
                 {
                     for (int x=lim; x<= b.Width-1 ; ++x)
                     {
-                        //c1 = pY[3] * pY[3];
-                        //c2 = pX[3] * pX[3];
-                        //nPixel = ((float)Math.Sqrt(c1 + c2));
-                        //if (nPixel > 255) nPixel = 255;
-                        //else if (nPixel < 0) nPixel = 0;
-                        //Grade[x+3,y] = (byte)nPixel;
-
-                        //c1 = pY[2] * pY[2];
-                        //c2 = pX[2] * pX[2];
-                        //nPixel = ((float)Math.Sqrt(c1 + c2));
-                        //if (nPixel > 255) nPixel = 255;
-                        //else if (nPixel < 0) nPixel = 0;
-                        //Grade[x+2,y] = (byte)nPixel;
-
-                        //c1 = pY[1] * pY[1];
-                        //c2 = pX[1] * pX[1];
-                        //nPixel = ((float)Math.Sqrt(c1 + c2));
-                        //if (nPixel > 255) nPixel = 255;
-                        //else if (nPixel < 0) nPixel = 0;
-                        //Grade[x+1,y] = (byte)nPixel;
-
-                        c1 = ((pY[0] + pY[1] + pY[2] + pY[3]) / 4 )* ((pY[0] + pY[1] + pY[2] + pY[3]) / 4); 
-                        c2 = ((pX[0] + pX[1] + pX[2] + pX[3]) / 4 )* ((pX[0] + pX[1] + pX[2] + pX[3]) / 4);
+                        
+                        c1 = aDivX[x, y] * aDivX[x,y];
+                        c2 = aDivY[x, y] * aDivY[x,y];
                         nPixel = ((float)(Math.Sqrt(c1 +c2)));
-                        if (nPixel > 255) nPixel = 255;
-                        else if (nPixel < 0) nPixel = 0;
-                        Grade[x,y] = (byte)nPixel;
+                        Grade[x,y] = nPixel;
 
-                        pY += 4;
-                        pX += 4;
-                        //pY++;
-                        //pX++;
+
                     }
-                    pY += nOffsetY;
-                    pX += nOffsetX;
+
                 }
 
                 //Non Maximum suppression
@@ -151,16 +132,23 @@ namespace CannyDetection
                 pX = (byte*)(void*)Scan0X;
                 pGrade = (byte*)(void*)Scan0G;
 
+                for(int y =0; y<b.Height; y++)
+                {
+                    for(int x=0; x<b.Width; x++)
+                    {
+                        NMax[x, y] = Grade[x, y];
+                    }
+                }
+
                 //Looping through the bitmap, x and y are used as refrences.
                 //We lose one pixel around the image, because we are using a 3x3
                 //to calculate
                 for (int y = lim; y<= nHeightN; y++) {
                     for (int x = lim; x <= nWidthN; x++) {
 
-                        //c1 = pX[pixleSize + strideG] + pX[1 + pixleSize + strideG] + pX[2 + pixleSize + strideG] + pX[3 + pixleSize + strideG];
-                        //c2 = pY[pixleSize + strideG] + pY[1 + pixleSize + strideG] + pY[2 + pixleSize + strideG] + pY[3 + pixleSize + strideG];
-                        c1 = (pX[0] + pX[1] + pX[2] + pX[3])/4;
-                        c2 = (pY[0] + pY[1] + pY[2] + pY[3])/4;
+
+                        c1 = aDivX[x, y];
+                        c2 = aDivY[x, y];
                         if (c1 == 0) {
                             tangent = 90F;
                         } else {
@@ -173,9 +161,6 @@ namespace CannyDetection
                         //rework the reffrencing, must change to reffrences to the top left
                         //most pixle corrisponding to the differentiator.
 
-                        //c1 = (Grade[pixleSize + strideG,y] + pGrade[pixleSize + 1 + strideG] + pGrade[pixleSize + 2 + strideG] + pGrade[3 + pixleSize + strideG]);
-                        //c2 = (pGrade[pixleSize] + pGrade[1 + pixleSize] + pGrade[2 + pixleSize] + pGrade[3+pixleSize]);
-                        //c3 = (pGrade[pixleSize + 2 * strideG] + pGrade[1 + pixleSize + 2*strideG] + pGrade[2 + pixleSize + 2*strideG] + pGrade[3 + pixleSize + 2*strideG]);
                         c1 = Grade[x, y];
                         c2 = Grade[x, y + 1];
                         c3 = Grade[x, y - 1];
@@ -184,17 +169,11 @@ namespace CannyDetection
                             if (c1 < c2 || c1 < c3)
                             {
                                 NMax[x, y] = 0;
-                                //pN[pixleSize + strideG] = 0;
-                                //pN[1 + pixleSize + strideG] = 0;
-                                //pN[2 + pixleSize + strideG] = 0;
-                                //pN[3 + pixleSize + strideG] = 0;
                             
                             }
                         }
 
                         //Vertical Edge, needs to calculate pixels above and bellow
-                        //c2 = (pGrade[0 + strideG] + pGrade[1 + strideG] + pGrade[2 + strideG] + pGrade[3 + strideG]);
-                        //c3 = (pGrade[2 * pixleSize + strideG] + pGrade[1 + 2 * pixleSize + strideG] + pGrade[2 + 2 * pixleSize + strideG] + pGrade[3 + 2*pixleSize + strideG]);
                         c2 = Grade[x + 1, y];
                         c3 = Grade[x - 1, y];
                         if ((-112.5 < tangent) && (-67.5 <= tangent) || (67.5 < tangent) && (tangent <= 112.5))
@@ -202,16 +181,11 @@ namespace CannyDetection
                             if (c1 < c2 || c1 < c3)
                             {
                                 NMax[x,y] = 0;
-                                //pN[pixleSize + strideG] = 0;
-                                //pN[1 + pixleSize + strideG] = 0;
-                                //pN[2 + pixleSize + strideG] = 0;
-                                //pN[3 + pixleSize + strideG] = 0;
+
                             }
                         }
 
                         //+45 Degree Edge
-                        //c2 = (pGrade[2*pixleSize] + pGrade[1 + 2*pixleSize] + pGrade[2 + 2*pixleSize] + pGrade[3+ 2*pixleSize]);
-                        //c3 = (pGrade[2*strideG] + pGrade[1 + 2*strideG] + pGrade[2 + 2*strideG] + pGrade[3 + 2*strideG]);
                         c2 = Grade[x + 1, y - 1];
                         c3 = Grade[x - 1, y + 1];
                         if ((-67.5<tangent)&&(-22.5<=tangent) || (112.5<tangent)&&(tangent<=157.5))
@@ -219,16 +193,11 @@ namespace CannyDetection
                             if (c1 < c2 || c1 < c3)
                             {
                                 NMax[x, y] = 0;
-                                //pN[pixleSize + strideG] = 0;
-                                //pN[1 + pixleSize + strideG] = 0;
-                                //pN[2 + pixleSize + strideG] = 0;
-                                //pN[3 + pixleSize + strideG] = 0;
+
                             }
                         }
 
                         //-45 Degree Edge
-                        //c2 = pGrade[0] + pGrade[1] + pGrade[2] + pGrade[3];
-                        //c3 = pGrade[2*pixleSize + 2* strideG] + pGrade[1 + 2*pixleSize + 2*strideG] + pGrade[2 + 2*pixleSize + 2*strideG] + pGrade[3+ 2*pixleSize + 2*strideG];
                         c2 = Grade[x + 1, y + 1];
                         c3 = Grade[x - 1, y - 1];
                         if ((-157.5<tangent)&&(-112.5<=tangent) || (67.5<tangent)&&(tangent<=22.5))
@@ -236,31 +205,17 @@ namespace CannyDetection
                             if (c1<c2 || c1<c3)
                             {
                                 NMax[x, y] = 0;
-                                //pN[pixleSize + strideG] = 0;
-                                //pN[1 + pixleSize + strideG] = 0;
-                                //pN[2 + pixleSize + strideG] = 0;
-                                //pN[3 + pixleSize + strideG] = 0;
+
                             }
                         }
-
-                        NMax[x, y] = Grade[x, y];
 
                         c1 = 0;
                         c2 = 0;
                         c3 = 0;
 
-                        //pGrade += 4;
-                        //pN += 4;
-                        pY += 4;
-                        pX += 4;
                     }
-                    //pGrade += nOffsetG;
-                    //pN += nOffsetN;
-                    pX += nOffsetX;
-                    pY += nOffsetY;
                 }
 
-                float[,] postHyst = new float[b.Width, b.Height];
 
                 postHyst = NMax.Clone() as float[,];
 
@@ -282,15 +237,20 @@ namespace CannyDetection
                     }
                 }
 
-                float MaxHyst = 20f;
-                float MinHyst = 10f;
-                float[,] Edges = new float[b.Width, b.Height];
+                EdgeMap = new int[b.Width, b.Height];
+                Visited = new int[b.Width, b.Height];
+
+                float MaxHyst = 80f;
+                //float MinHyst = 10f;
 
                 for (int x = 1; x< b.Width - 1; x++)
                 {
                     for (int y = 1; y<b.Height - 1; y++)
                     {
-
+                        if(postHyst[x,y] >= MaxHyst )
+                        {
+                            Edges[x, y] = 1;
+                        }
                     }
                 }
             }//end of unsafe code
@@ -299,8 +259,9 @@ namespace CannyDetection
             divX.UnlockBits(divXData);
             grade.UnlockBits(gradeData);
 
-            NonMax = convertToBitmap(Grade, b.Width, b.Height);
+            threshold(Edges, b.Width, b.Height);
 
+            NonMax = convertToBitmap(EdgeMap, b.Width, b.Height);
 
             return NonMax;
         }
@@ -309,7 +270,8 @@ namespace CannyDetection
         {
             Bitmap bits= new Bitmap(width, height);
             BitmapData bitsD = bits.LockBits(new Rectangle(0, 0, width, height),
-                                            ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                                            ImageLockMode.ReadOnly, 
+                                            PixelFormat.Format32bppArgb);
 
             unsafe {
                 byte* p = (byte*)(void*)bitsD.Scan0;
@@ -321,7 +283,7 @@ namespace CannyDetection
                         p[0] = (byte)((int)src[x, y]);
                         p[1] = (byte)((int)src[x, y]);
                         p[2] = (byte)((int)src[x, y]);
-                        p[3] = (byte)((int)src[x, y]);
+                        p[3] = (byte)((int)255);
 
                         p += 4;
                     }
@@ -331,6 +293,148 @@ namespace CannyDetection
             bits.UnlockBits(bitsD);
 
             return bits;
+        }
+
+        public static Bitmap convertToBitmap(int[,] src, int w, int h)
+        {
+            Bitmap bits = new Bitmap(w, h);
+            BitmapData bitsD = bits.LockBits(new Rectangle(0, 0, w, h),
+                                             ImageLockMode.ReadOnly,
+                                             PixelFormat.Format32bppArgb);
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)bitsD.Scan0;
+
+                for (int y = 0; y < h; y++)
+                {
+                    for (int x = 0; x < w; x++)
+                    {
+                        p[0] = (byte)((int)src[x, y]);
+                        p[1] = (byte)((int)src[x, y]);
+                        p[2] = (byte)((int)src[x, y]);
+                        p[3] = (byte)((int)255);
+
+                        p += 4;
+                    }
+                    p += (bitsD.Stride - (bitsD.Width * 4));
+                }
+
+            }
+            bits.UnlockBits(bitsD);
+            return bits;
+
+        }
+
+        public static int[,] convertToArray(Bitmap b)
+        {
+            int[,] res = new int[b.Width, b.Height];
+            BitmapData bData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+                                            ImageLockMode.ReadOnly,
+                                            PixelFormat.Format32bppArgb);
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)bData.Scan0;
+
+                for(int y=0; y<b.Height; y++)
+                {
+                    for(int x=0; x<b.Width; x++)
+                    {
+                        res[x, y] = (int)((p[0] + p[1] + p[2])/3.0);
+                        p += 4;
+                    }
+                    p += (bData.Stride - (bData.Width * 4));
+                }
+            }
+            b.UnlockBits(bData);
+
+            return res;
+        }
+
+        public static void threshold(int[,] Edges, int w, int h)
+        {
+            for(int x=1; x<=w-2; x++)
+            {
+                for(int y=1; y<=h-2; y++)
+                {
+                    if(Edges[x,y] == 1)
+                    {
+                        travers(x, y);
+                        EdgeMap[x, y] = 255;
+                        Visited[x, y] = 1;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        private static void travers(int x, int y)
+        {
+            if(Visited[x,y] == 1)
+            {
+                return;
+            }
+
+            if(Edges[x + 1,y] == 1)
+            {
+                EdgeMap[x + 1, y] = 255;
+                Visited[x + 1, y] = 1;
+                travers(x + 1, y);
+                return ;
+            }
+            if (Edges[x + 1, y - 1] == 1)
+            {
+                EdgeMap[1, y - 1] = 255;
+                Visited[x + 1, y - 1] = 1;
+                travers(x + 1, y - 1);
+                return;
+            }
+            if (Edges[x, y - 1] == 1)
+            {
+                EdgeMap[x, y - 1] = 255;
+                Visited[x, y - 1] = 1;
+                travers(x, y - 1);
+                return;
+            }
+            if (Edges[x - 1, y - 1] == 1)
+            {
+                EdgeMap[x - 1, y - 1] = 255;
+                Visited[x - 1, y - 1] = 1;
+                travers(x - 1, y - 1);
+                return;
+            }
+            if (Edges[x - 1, y] == 1)
+            {
+                EdgeMap[x - 1, y] = 255;
+                Visited[x - 1, y] = 1;
+                travers(x - 1, y);
+                return;
+            }
+            if (Edges[x - 1, y + 1] == 1)
+            {
+                EdgeMap[x - 1, y + 1] = 255;
+                Visited[x - 1, y + 1] = 1;
+                travers(x - 1, y + 1);
+                return;
+            }
+            if (Edges[x, y + 1] == 1)
+            {
+                EdgeMap[x, y + 1] = 255;
+                Visited[x, y + 1] = 1;
+                travers(x, y + 1);
+                return;
+            }
+            if (Edges[x + 1, y + 1] == 1)
+            {
+                EdgeMap[x + 1, y + 1] = 255;
+                Visited[x + 1, y + 1] = 1;
+                travers(x + 1, y + 1);
+                return;
+            }
+
+            return;
         }
 
     }
